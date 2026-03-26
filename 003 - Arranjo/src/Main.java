ea35158379d7be5b72050aa0331cf6b3b4ff7a78
EduardoBougo <eduardobougo@gmail.com>
2026-03-25 22:14:28 -0300

    a

---
+++ 003 - Arranjo/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int [] primos = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+        double [] a = new double[10];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Math.random();
+        }
+
+        Arranjo.printArray(a);
+
+        System.out.println("=================================");
+
+        Arranjo.printArray(primos);
+
+        Arranjo.printMaxAndMin(primos);
+
+        System.out.println("=================================");
+
+        Arranjo.printReverseArray(primos);
+    }
+}
