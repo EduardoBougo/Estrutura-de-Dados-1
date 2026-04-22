b383140726fd5fd5a7290b1ccc94b6d987e319a2
EduardoBougo <eduardobougo@gmail.com>
2026-04-22 15:05:44 -0300

    Testes internos

--- 010 - Node - Duplamente Encadeado/src/Main.java
+++ 010 - Node - Duplamente Encadeado/src/Main.java
@@ -9,6 +9,12 @@ public class Main {
         DNode node_4 = new DNode("JKL", null, null);
         DNode node_5 = new DNode("MNO", null, null);
 
+        DNode node_6 = new DNode("PQR", null, null);
+        DNode node_7 = new DNode("TUV", null, null);
+        DNode node_8 = new DNode("WXYZ", null, null);
+        DNode node_9 = new DNode("XYZ", null, null);
+        DNode node_10 = new DNode("ZXY", null, null);
+
         listadupla.addLast(node_5);
         listadupla.addLast(node_4);
         listadupla.addLast(node_3);
@@ -19,8 +25,23 @@ public class Main {
 
 
         // listadupla.remove("def");
+        DList lista_teste =  new DList();
+        lista_teste.addLast(node_6);
+        lista_teste.addLast(node_7);
+        lista_teste.addLast(node_8);
+        lista_teste.addLast(node_9);
+        lista_teste.addLast(node_10);
+
         listadupla.insertionSort(listadupla);
         System.out.println(listadupla.toString());
+
+        System.out.println("=======================================");
+
+        DList listaSomada = new DList();
+
+        listaSomada.linkLists(listadupla, lista_teste);
+
+        System.out.println(listaSomada.toString());
 
     }
 }
