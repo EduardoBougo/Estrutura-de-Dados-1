3a2e8260ddb76756e082f1bc26d808cf6c1322b1
EduardoBougo <eduardobougo@gmail.com>
2026-06-23 12:16:39 -0300

    a

---
+++ 017 - Arvore Binaria/src/BinaryTree.java
@@ -0,0 +1,34 @@
+//begin#fragment Tree
+
+/**
+ * An interface for a binary tree, where each node can have zero, one,
+ * or two children.
+ * //end#fragment Tree
+ *
+ * @author Michael Goodrich //begin#fragment Tree
+ */
+public interface BinaryTree<E> extends Tree<E> {
+
+    /**
+     * Returns the left child of a node.
+     */
+    public Position<E> left(Position<E> v)
+            throws IllegalArgumentException;
+
+    /**
+     * Returns the right child of a node.
+     */
+    public Position<E> right(Position<E> v)
+            throws IllegalArgumentException;
+
+    /**
+     * Returns whether a node has a left child.
+     */
+    public boolean hasLeft(Position<E> v) throws IllegalArgumentException;
+
+    /**
+     * Returns whether a node has a right child.
+     */
+    public boolean hasRight(Position<E> v) throws IllegalArgumentException;
+}
+//end#fragment Tree
---
+++ 017 - Arvore Binaria/src/DNode.java
@@ -0,0 +1,54 @@
+
+/**
+ * A simple node class for a doubly-linked list.  Each DNode has a
+ * reference to a stored element, a previous node, and a next node.
+ *
+ * @author Roberto Tamassia
+ */
+//begin#fragment DNode
+public class DNode<E> implements Position<E> {
+
+    DNode<E> prev;
+    protected DNode<E> next; // References to the nodes before and after
+    protected E element;	// Element stored in this position
+
+    /**
+     * Constructor
+     */
+    public DNode(DNode<E> newPrev, DNode<E> newNext, E elem) {
+        prev = newPrev;
+        next = newNext;
+        element = elem;
+    }
+    // Method from interface Position
+
+    public E getElement() throws IllegalArgumentException {
+        if ((prev == null) && (next == null)) {
+            throw new IllegalArgumentException("Position is not in a list!");
+        }
+        return element;
+    }
+    // Accessor methods
+
+    public DNode<E> getNext() {
+        return next;
+    }
+
+    public DNode<E> getPrev() {
+        return prev;
+    }
+    // Update methods
+
+    public void setNext(DNode<E> newNext) {
+        next = newNext;
+    }
+
+    public void setPrev(DNode<E> newPrev) {
+        prev = newPrev;
+    }
+
+    public void setElement(E newElement) {
+        element = newElement;
+    }
+}
+//end#fragment DNode
